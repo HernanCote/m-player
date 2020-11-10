4880f74b20e48cc90aaec4daf00ab277fac165c9
Hernan Cote <hernan.cote@zemoga.com>
2020-11-10 16:09:55 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Song from './components/Song';
+import Player from './components/Player';
+
+import './styles/app.scss';
+
+const App = () => (
+	<div className="App">
+		<Song />
+		<Player />
+	</div>
+);
+
+export default App;
